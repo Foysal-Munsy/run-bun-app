cd34a0f68ed7702a2ba2a0f91fed1a5c28201c0e
Foysal-Munsy <bdfoysal555@gmail.com>
2026-01-19 19:50:41 +0600

    [added] scalar api documentation /reference.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,14 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  // SwaggerModule.setup('api', app, documentFactory);
+
+  app.use(
+    '/reference',
+    apiReference({
+      content: documentFactory,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3005);
 }
